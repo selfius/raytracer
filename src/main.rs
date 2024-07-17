b37b8a5275da6f879035b2b71455002782cd69aa
Andrei Sviridov <self.polaris@gmail.com>
2024-07-16 20:37:33 -0400

    Refactor Vector to use Add, Sub and Mul traits

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,8 @@ const WIDTH: u32 = 1024;
 const HEIGHT: u32 = 768;
 const CHANNELS: u8 = 3;
 
+const HORIZONTAL_FOV: f32 = 90.0;
+
 fn main() -> io::Result<()> {
     let output = File::create("test.png")?;
     let mut encoder = Encoder::new(output, WIDTH, HEIGHT);
@@ -38,15 +40,14 @@ fn draw(buffer: &mut Buffer) {
 
     // camera is set up so that it looks in (0,0,-1) direction
     // with the virtual screen being always one unit away from camera.
-    let horizontal_fov = 90.0;
-    let vertical_fov =
-        f32::atan(HEIGHT as f32 * f32::tan(f32::to_radians(horizontal_fov / 2.0)) / WIDTH as f32)
-            .to_degrees()
-            * 2.0;
+    let vertical_fov = (HEIGHT as f32 * (HORIZONTAL_FOV / 2.0).to_radians().tan() / WIDTH as f32)
+        .atan()
+        .to_degrees()
+        * 2.0;
 
     // virtual screen size in world coordinates
-    let in_world_screen_width = 2.0 * f32::tan(f32::to_radians(horizontal_fov / 2.0));
-    let in_world_screen_height = 2.0 * f32::tan(f32::to_radians(vertical_fov / 2.0));
+    let in_world_screen_width = 2.0 * (HORIZONTAL_FOV /2.0).to_radians().tan();
+    let in_world_screen_height= 2.0 * (vertical_fov /2.0).to_radians().tan();
 
     // size of a pixel in the output translated to world coordinates
     let in_world_pixel_x_offset = Vec3::new(in_world_screen_width / (WIDTH as f32), 0.0, 0.0);
@@ -58,32 +59,31 @@ fn draw(buffer: &mut Buffer) {
     let forward_vector = Vec3::new(0.0, 0.0, -1.0);
 
     // top left of the virtual screen
-    let in_world_top_left = camera_position.clone().add(&Vec3::new(
-        -in_world_screen_width / 2.0,
-        in_world_screen_height / 2.0,
-        -1.0,
-    ));
+    let in_world_top_left = camera_position
+        + Vec3::new(
+            -in_world_screen_width / 2.0,
+            in_world_screen_height / 2.0,
+            -1.0,
+        );
 
     for x in 0..WIDTH {
         for y in 0..HEIGHT {
             // mapping between the pixel on the png and the vector looking at its representation
             // on the virtual screen from the pov of the camera
-            let camera_to_pixel_direction = in_world_pixel_x_offset
-                .clone()
-                .multiply(x as f32)
-                .add(&in_world_pixel_y_offset.clone().multiply(y as f32))
-                .add(&in_world_top_left)
-                .add(&camera_position.clone().multiply(-1.0))
+            let camera_to_pixel_direction = (in_world_pixel_x_offset * x as f32
+                + in_world_pixel_y_offset * y as f32
+                + in_world_top_left
+                - camera_position)
                 .normalize();
 
             let angle = f32::acos(
-                forward_vector.dot_product(&camera_to_pixel_direction)
-                    / (forward_vector.magnitude() * camera_to_pixel_direction.magnitude()),
+                (forward_vector * camera_to_pixel_direction) / forward_vector.magnitude()
+                    * camera_to_pixel_direction.magnitude(),
             )
             .to_degrees();
 
             if angle < 10.0 {
-                // drawing the circle that is the intersection of all vertices comming from the
+                // drawing the circle that is the intersection of all vectors comming from the
                 // camera that form angle less than 10 degrees with the camera view direction and
                 // the virtual screen plane
                 buffer.set(Point(x, y), Rgb(100, 100, 100));
--- src/vector_math.rs
+++ src/vector_math.rs
@@ -1,34 +1,52 @@
-#[derive(Debug, Clone)]
+use std::ops::{Add, Mul, Sub};
+
+#[derive(Debug, Clone, Copy)]
 pub struct Vec3 {
     values: [f32; 3],
+}
+
+impl Add for Vec3 {
+    type Output = Self;
+    fn add(self, v2: Self) -> Self {
+        let mut new_values = [0.0; 3];
+        for (idx, (v1_i, v2_i)) in self.values.iter().zip(v2.values).enumerate() {
+            new_values[idx] = v1_i + v2_i;
+        }
+        Self { values: new_values }
+    }
+}
+
+impl Mul<f32> for Vec3 {
+    type Output = Self;
+    fn mul(mut self, rhs: f32) -> Self {
+        for i in &mut self.values {
+            *i *= rhs;
+        }
+        self
+    }
+}
+
+impl Mul<Vec3> for Vec3 {
+    type Output = f32;
+    fn mul(self, rhs: Vec3) -> f32 {
+        let mut product = 0f32;
+        for (v1_i, v2_i) in self.values.iter().zip(rhs.values.iter()) {
+            product += v1_i * v2_i
+        }
+        product
+    }
+}
+
+impl Sub<Vec3> for Vec3 {
+    type Output = Self;
+    fn sub(self, rhs: Vec3) -> Self {
+        self + (rhs * -1.0)
+    }
 }
 
 impl Vec3 {
     pub fn new(x: f32, y: f32, z: f32) -> Vec3 {
         Vec3 { values: [x, y, z] }
-    }
-
-    pub fn add(self, v2: &Vec3) -> Vec3 {
-        let mut new_values = [0.0; 3];
-        for (idx, (v1_i, v2_i)) in self.values.iter().zip(v2.values).enumerate() {
-            new_values[idx] = v1_i + v2_i;
-        }
-        Vec3 { values: new_values }
-    }
-
-    pub fn dot_product(&self, v2: &Vec3) -> f32 {
-        let mut product = 0f32;
-        for (v1_i, v2_i) in self.values.iter().zip(v2.values.iter()) {
-            product += v1_i * v2_i
-        }
-        product
-    }
-
-    pub fn multiply(mut self, scalar: f32) -> Vec3 {
-        for i in &mut self.values {
-            *i *= scalar;
-        }
-        self
     }
 
     pub fn magnitude(&self) -> f32 {
@@ -41,6 +59,6 @@ impl Vec3 {
 
     pub fn normalize(self) -> Vec3 {
         let magnitude = self.magnitude();
-        self.multiply(1.0 / magnitude)
+        self * (1.0 / magnitude)
     }
 }
