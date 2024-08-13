4723d3eaf3b1a2dc9ede3edac0c8c053971b02b8
Andrei Sviridov <self.polaris@gmail.com>
2024-08-13 16:43:33 -0400

    Convert project structure to cargo workspace

--- image_gen/src/main.rs
+++ image_gen/src/main.rs
@@ -1,20 +1,17 @@
+use lib::buffer::{Buffer, Dimensions};
 use png::Encoder;
-use raytracer::buffer::{Buffer, Dimensions};
 use std::{fs::File, io};
 
 fn main() -> io::Result<()> {
     let output = File::create("test.png")?;
-    let mut encoder = Encoder::new(output, raytracer::WIDTH, raytracer::HEIGHT);
+    let mut encoder = Encoder::new(output, lib::WIDTH, lib::HEIGHT);
 
     encoder.set_color(png::ColorType::Rgb);
     encoder.set_depth(png::BitDepth::Eight);
 
-    let mut buffer = Buffer::new(
-        Dimensions(raytracer::WIDTH, raytracer::HEIGHT),
-        raytracer::CHANNELS,
-    );
+    let mut buffer = Buffer::new(Dimensions(lib::WIDTH, lib::HEIGHT), lib::CHANNELS);
     buffer.clear();
-    raytracer::draw(&mut buffer);
+    lib::draw(&mut buffer);
 
     let mut writer = encoder.write_header()?;
     writer.write_image_data(buffer.get_data_ref())?;
